d2a9e8dab895692c4388e67cbca3a0d7efde2883
nico <nico.garming@brockmann-consult.de>
2013-09-20 13:05:10 +0100

    Werte in eine neue Datei

--- XML-Datei_programmieren/logic/catalogue_XML.py
+++ XML-Datei_programmieren/logic/catalogue_XML.py
@@ -1,58 +1,59 @@
 __author__ = 'nico'
 
 f = open('../input/ATS_TOA_1PRBCM20030102_113342_000000482012_00337_04399_0003.N1', 'r')
+output = open('../output/catalogue_xml.xml', 'w')
 
 for i in range(16):
       line = f.readline()
+      write = output.write
 
       a = 'PRODUCT='
       if line.startswith(a):
           startIndex = line.index('\"')+1
           endIndex = len(line)-2
-          print(line[startIndex:endIndex])
-
+          write(line[startIndex:endIndex])
 
       b = 'ACQUISITION_STATION='
       if line.startswith(b):
           startIndex = line.index('\"')+1
           endIndex = len(line)-2
-          print(line[startIndex:endIndex])
+          write(line[startIndex:endIndex])
 
       c = 'PROC_CENTER='
       if line.startswith(c):
           startIndex = line.index('\"')+1
           endIndex = len(line)-2
-          print(line[startIndex:endIndex])
+          write(line[startIndex:endIndex])
 
       d = 'PROC_TIME='
       if line.startswith(d):
           startIndex = line.index('\"')+1
           endIndex = len(line)-2
-          print(line[startIndex:endIndex])
+          write(line[startIndex:endIndex])
 
       e = 'SOFTWARE_VER='
       if line.startswith(e):
           startIndex = line.index('\"')+1
           endIndex = len(line)-2
-          print(line[startIndex:endIndex])
+          write(line[startIndex:endIndex])
 
       j = 'SENSING_START='
       if line.startswith(j):
           startIndex = line.index('\"')+1
           endIndex = len(line)-2
-          print(line[startIndex:endIndex])
+          write(line[startIndex:endIndex])
 
       g = 'SENSING_STOP='
       if line.startswith(g):
           startIndex = line.index('\"')+1
           endIndex = len(line)-2
-          print(line[startIndex:endIndex])
+          write(line[startIndex:endIndex])
 
       h = 'ABS_ORBIT='
       if line.startswith(h):
           startIndex = line.index('=')+1
           endIndex = len(line)-1
-          print(line[startIndex:endIndex])
+          write(line[startIndex:endIndex])
 
 
 
