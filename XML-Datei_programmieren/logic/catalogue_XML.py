2d51638ca6b6d503fb2ce34bad6e86204947d0cd
nico <nico.garming@brockmann-consult.de>
2013-09-20 10:24:42 +0100

    Aufgabe des Kunden

--- XML-Datei_programmieren/logic/catalogue_XML.py
+++ XML-Datei_programmieren/logic/catalogue_XML.py
@@ -2,9 +2,42 @@ __author__ = 'nico'
 
 f = open('../input/ATS_TOA_1PRBCM20030102_113342_000000482012_00337_04399_0003.N1', 'r')
 
-for i in range(30):
-      x = f.readline()
-      print(x)
+for i in range(16):
+      line = f.readline()
+
+
+      a = 'PRODUCT='
+      if line.startswith(a):
+          print(line)
+
+      b = 'ACQUISITION_STATION='
+      if line.startswith(b):
+          print(line)
+
+      c = 'PROC_CENTER='
+      if line.startswith(c):
+          print(line)
+
+      d = 'PROC_TIME='
+      if line.startswith(d):
+          print(line)
+
+      e = 'SOFTWARE_VER='
+      if line.startswith(e):
+          print(line)
+
+      j = 'SENSING_START='
+      if line.startswith(j):
+          print(line)
+
+      g = 'SENSING_STOP='
+      if line.startswith(g):
+          print(line)
+
+      h = 'ABS_ORBIT='
+      if line.startswith(h):
+          print(line)
+
 
 
 f.close()
