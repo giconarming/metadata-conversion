f45759a01695bab4cd4a2e48aea3664e3c7297a4
nico <nico.garming@brockmann-consult.de>
2013-09-26 14:17:17 +0100

    ERROR bei fehler in der console

--- XML-Datei_programmieren/logic/catalogue_XML1.py
+++ XML-Datei_programmieren/logic/catalogue_XML1.py
@@ -6,28 +6,31 @@ import sys
 f = open(sys.argv[1], 'r')
 output = open('../output/catalogue.xml', 'w')
 write = output.write
-write('<catalog_entry>')
-write('\n')
+write('<catalog_entry>\n')
 
-for i in range(75):
-    line = f.readline()
-    gleich = line.find('=')
-    anführungs = line.find('"')
-    tag = line[0:gleich]
+try:
+    for i in range(75):
+        line = f.readline()
+        gleich = line.find('=')
+        anfuehrungs = line.find('"')
+        tag = line[0:gleich]
 
-    if anführungs >=0:
-        wertende = len(line)-2
-        wertanfang = gleich+2
-    else:
-        wertende = len(line)-1
-        wertanfang = gleich+1
+        if anfuehrungs >=0:
+            wertende = len(line)-2
+            wertanfang = gleich+2
+        else:
+            wertende = len(line)-1
+            wertanfang = gleich+1
 
 
-    if gleich >=0:
-        write('   ')
-        write('<'+tag.lower()+'>')
-        write(escape(line[wertanfang:wertende].strip()))
-        write('</'+tag.lower()+'>\n')
+        if gleich >=0:
+            write('   ')
+            write('<'+tag.lower()+'>')
+            write(escape(line[wertanfang:wertende].strip()))
+            write('</'+tag.lower()+'>\n')
+except Exception as ex:
+    print('ERROR\n')
+    print('can\'t read the file!\n')
 
 write('</catalog_entry>')
 f.close()
