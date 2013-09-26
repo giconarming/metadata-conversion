065b9f25f8357e4ac06a6bf39d2b9de45446f52c
nico <nico.garming@brockmann-consult.de>
2013-09-26 14:41:20 +0100

    if startswith(product)

--- XML-Datei_programmieren/logic/catalogue_XML1.py
+++ XML-Datei_programmieren/logic/catalogue_XML1.py
@@ -3,34 +3,42 @@ from xml.sax.saxutils import escape
 import sys
 
 
-f = open(sys.argv[1], 'r')
+input = open(sys.argv[1], 'r')
 output = open('../output/catalogue.xml', 'w')
 write = output.write
 write('<catalog_entry>\n')
+product ='PRODUCT='
 
 try:
-    for i in range(75):
-        line = f.readline()
-        gleich = line.find('=')
-        anfuehrungs = line.find('"')
-        tag = line[0:gleich]
+    line = input.readline()
+    if line.startswith(product):
+        for i in range(75):
+            line = input.readline()
+            gleich = line.find('=')
+            anfuehrungs = line.find('"')
+            tag = line[0:gleich]
 
-        if anfuehrungs >=0:
-            wertende = len(line)-2
-            wertanfang = gleich+2
-        else:
-            wertende = len(line)-1
-            wertanfang = gleich+1
+            if anfuehrungs >=0:
+                wertende = len(line)-2
+                wertanfang = gleich+2
+            else:
+                wertende = len(line)-1
+                wertanfang = gleich+1
 
 
-        if gleich >=0:
-            write('   ')
-            write('<'+tag.lower()+'>')
-            write(escape(line[wertanfang:wertende].strip()))
-            write('</'+tag.lower()+'>\n')
+            if gleich >=0:
+                write('   ')
+                write('<'+tag.lower()+'>')
+                write(escape(line[wertanfang:wertende].strip()))
+                write('</'+tag.lower()+'>\n')
+    else:
+        pass
+
+
 except Exception as ex:
     print('ERROR\n')
     print('can\'t read the file!\n')
 
 write('</catalog_entry>')
-f.close()
+input.close()
+output.close()
